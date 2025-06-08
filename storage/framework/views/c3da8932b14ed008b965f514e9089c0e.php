ea5739f2a001c7444baccfe7fa4ca180efe34bac
Nur Fazreen Laila <78629627+laila772@users.noreply.github.com>
2025-06-09 02:39:03 +0800

    tak guna vite

--- storage/framework/views/c3da8932b14ed008b965f514e9089c0e.php
+++ storage/framework/views/c3da8932b14ed008b965f514e9089c0e.php
@@ -9,9 +9,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('css/home.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('css/bootstrap.min.css')); ?>">
     <link href="<?php echo e(asset('css/bootstrap-icons.css')); ?>" rel="stylesheet">
-    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
-
+    <link rel="stylesheet" href="<?php echo e(asset('css/templatemo-tiya-golf-club.css')); ?>">
 
 </head>
 <body>
