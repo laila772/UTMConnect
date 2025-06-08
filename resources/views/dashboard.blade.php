411806ccc3677660640901318d48e5067fa5125d
Nur Fazreen Laila <78629627+laila772@users.noreply.github.com>
2025-06-09 01:15:38 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
-<head>
+<!-- <head>
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-</head>
+</head> -->
 
 <main>
 
