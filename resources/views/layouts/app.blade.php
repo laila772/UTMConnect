9e187217e3ba551e7fcd84575962e71a0ae7fb8c
Nur Fazreen Laila <78629627+laila772@users.noreply.github.com>
2025-06-09 01:09:26 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/templatemo-tiya-golf-club.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
 </head>
 <body>
