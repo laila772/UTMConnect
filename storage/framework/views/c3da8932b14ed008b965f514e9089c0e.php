19b4a133871298348d8e06aff8476eadb1e777cb
Nur Fazreen Laila <78629627+laila772@users.noreply.github.com>
2025-06-09 02:31:35 +0800

    500 error

---
+++ storage/framework/views/c3da8932b14ed008b965f514e9089c0e.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $__env->yieldContent('title', 'UTMConnect+'); ?></title>
+
+    <!-- CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="<?php echo e(asset('css/bootstrap-icons.css')); ?>" rel="stylesheet">
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+
+
+</head>
+<body>
+    
+
+    <main class="content">
+        <?php echo $__env->yieldContent('content'); ?>
+    </main>
+
+    
+
+    <!-- JAVASCRIPT FILES -->
+<script src="<?php echo e(asset('js/jquery.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/bootstrap.bundle.min.js')); ?>"></script>
+<script src="<?php echo e(asset('js/jquery.sticky.js')); ?>"></script>
+<script src="<?php echo e(asset('js/click-scroll.js')); ?>"></script>
+<script src="<?php echo e(asset('js/animated-headline.js')); ?>"></script>
+<script src="<?php echo e(asset('js/modernizr.js')); ?>"></script>
+<script src="<?php echo e(asset('js/custom.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\cuba\UTMConnect\resources\views/layouts/app.blade.php ENDPATH**/ ?>
