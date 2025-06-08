76c3671a54d03f0c4afb391b4eda1145f7515ad0
Nur Fazreen Laila <78629627+laila772@users.noreply.github.com>
2025-06-09 01:17:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
+    base: '/build/',
     build: {
         outDir: 'public/build',
         manifest: true,
