83b21b70f7880a08c7c7c03037d973d38afd7234
Nur Fazreen Laila <78629627+laila772@users.noreply.github.com>
2025-06-09 13:25:06 +0800

    cuba for styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,13 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/templatemo-tiya-golf-club.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
     <link rel="stylesheet" href="https://utmconnect.onrender.com/css/dashboard.css">
+
+    <style>
+    body {
+        background-color: yellow !important;
+    }
+</style>
 
 </head>
 <body>
